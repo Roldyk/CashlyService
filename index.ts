684de392df32b28bf7d4a842516d7688b0664274
Francisco Madera <franciscomaderanatera@gmail.com>
2025-06-23 19:26:13 -0400

    feat: route de gastos

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import authRouter from './Routes/authRouter';
-import presupuestoRouter from './Routes/presupuestosRouter';
+import express from "express";
+import authRouter from "./Routes/authRouter";
+import presupuestoRouter from "./Routes/presupuestosRouter";
+import gastosRouter from "./Routes/gastosRouter";
 
 const app = express();
 const port = 3000;
-import { PrismaClient } from './generated/prisma';
-const prisma = new PrismaClient();
 
 app.use(express.json());
-app.use('/auth', authRouter);
-app.use('/presupuesto', presupuestoRouter);
+app.use("/auth", authRouter);
+app.use("/presupuesto", presupuestoRouter);
+app.use("/gastos", gastosRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }); 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    app.listen(port, () => { 
-    console.log(`Server is running  at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running  at http://localhost:${port}`);
 });
